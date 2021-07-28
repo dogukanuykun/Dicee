836ea0f90b0b5905d34b5c6c8401b52717c935e6
Doğukan Uykun <76883066+dogukanuykun@users.noreply.github.com>
2021-07-28 15:23:41 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+var randomNumber = Math.floor(Math.random()*6) +1;
+var randomNumber2 = Math.floor(Math.random()*6) +1;
+
+if(randomNumber>randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+}
+else if(randomNumber2>randomNumber){
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+}
+else{
+    document.querySelector("h1").innerHTML = "Draw!";
+}
+
+if(randomNumber===1){
+    document.querySelectorAll("img")[0].setAttribute("src" ,"./images/dice1.png")
+}
+if(randomNumber===2){
+    document.querySelectorAll("img")[0].setAttribute("src" ,"./images/dice2.png")
+}
+if(randomNumber===3){
+    document.querySelectorAll("img")[0].setAttribute("src" ,"./images/dice3.png")
+}
+if(randomNumber===4){
+    document.querySelectorAll("img")[0].setAttribute("src" ,"./images/dice4.png")
+}
+if(randomNumber===5){
+    document.querySelectorAll("img")[0].setAttribute("src" ,"./images/dice5.png")
+}
+if(randomNumber===6){
+    document.querySelectorAll("img")[0].setAttribute("src" ,"./images/dice6.png")
+}
+
+// DICE 2
+
+if(randomNumber2===1){
+    document.querySelectorAll("img")[1].setAttribute("src" ,"./images/dice1.png")
+}
+if(randomNumber2===2){
+    document.querySelectorAll("img")[1].setAttribute("src" ,"./images/dice2.png")
+}
+if(randomNumber2===3){
+    document.querySelectorAll("img")[1].setAttribute("src" ,"./images/dice3.png")
+}
+if(randomNumber2===4){
+    document.querySelectorAll("img")[1].setAttribute("src" ,"./images/dice4.png")
+}
+if(randomNumber2===5){
+    document.querySelectorAll("img")[1].setAttribute("src" ,"./images/dice5.png")
+}
+if(randomNumber2===6){
+    document.querySelectorAll("img")[1].setAttribute("src" ,"./images/dice6.png")
+}
